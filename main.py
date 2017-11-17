73af52f20925192b81128eee55e4b8f9011bc0c6
chda6313 <chda6313@colorado.edu>
2017-11-17 09:11:14 -0700

    adding a matrix for storing what we have explored so far

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 from math import cos, sin
-resolution = 15
+resolution = 20
 pi = 3.14159
 
 #should read data in as: [X,Y,theta, alpha, range]
@@ -44,29 +44,64 @@ def scansToReal(scans):
 
 
 #convert points to a map for display
-def showMap(points):
+def showMap(points, x1, y1, x2, y2):#(list of obstacles, s1x, s1y, s2x, s2y)
 	worldMap = [['.' for x in range(-resolution, resolution+1)] for y in range(-resolution, resolution+1)]
 	for [x,y] in points:
 		worldMap[resolution+x][resolution+y] = 'O'   #O for obstacle
+	worldMap[resolution+x1][resolution+y1]="1"
+	worldMap[resolution+x2][resolution+y2]="2"
+
+	print("=========================================")
 	for row in [worldMap[len(worldMap)-1-i] for i in range(0, len(worldMap))]: 
 		r='|'
 		for char in row:
 			r += char
 		r+='|'
 		print(r)
-
+	print("=========================================")
 
 r1 = scansToReal(scans[0:7])
-showMap(r1)
+#showMap(r1, 0, 0, 5, 5)
 
-r2 = scansToReal(scans[8:14])
-showMap(r2)
+r2 = scansToReal(scans[8:])
+#showMap(r2, 0, 0, 5, 5)
 
-showMap(r1+r2)
-
-
+#showMap(r1+r2, 0, 0, 5, 5)
 
 
+r = resolution
+explored = [[0 for x in range(-r, r)] for y in range(0,2*r)] #2r * 2r map of what we have explored
+
+
+def exploreEnv(exploreMap, res, scans):
+	obs = []
+	exploredMap = exploreMap[:][:]
+	
+	for [rx,ry,t,a,r] in scans:#grab each datapoint
+		#convert the sensed obstacle to x and y
+		x = rx + r*cos(t+a)
+		y = ry+ r*sin(t+a)
+		obs.append([res+x,y])
+		
+		for d in range(0,r):
+			x = rx + d*cos(t+a)
+			y = ry + d*sin(t+a)
+			
+			exploredMap[int(res+x)][int(y)] = 1
+	return obs, exploredMap
+		
+
+o, explored = exploreEnv(explored, r, scans)
+
+
+print("=========================================")
+for row in [explored[len(explored)-1-i] for i in range(0, len(explored))]: 
+	r='|'
+	for char in row:
+		r += str(char)
+	r+='|'
+	print(r)
+print("=========================================")
 
 
 
@@ -74,3 +109,16 @@ showMap(r1+r2)
 
 
 
+
+
+#TODO
+
+#2 maps, obstacles & fog of war
+
+
+#dijk
+#integrate scanning code
+
+#Planning
+	#look at unexplored areas and try to path robots into them
+	#dijk only through explored
